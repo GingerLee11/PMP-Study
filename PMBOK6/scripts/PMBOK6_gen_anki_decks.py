a55648edc2f0030a0142b9e4707f0a54ed1c4d98
Scott <clemencyofcollege@gmail.com>
2025-06-09 13:24:50 -0400

    Update previous PMBOK6 script to import the models from models.py

--- PMBOK6/scripts/PMBOK6_gen_anki_decks.py
+++ PMBOK6/scripts/PMBOK6_gen_anki_decks.py
@@ -2,57 +2,13 @@ import genanki
 import pandas as pd
 import os
 
+# Import custom models
+from models import basic_model, cloze_model
+
 # Config
 CSV_DIR = r'PMBOK6\PMBOK6_cleaned_decks'
 OUTPUT_PACKAGE = r'PMBOK6\PMBOK6_Deck.apkg'
-MODEL_ID_BASIC = 1607392319
-MODEL_ID_CLOZE = 1782910741
 
-# Define models
-basic_model = genanki.Model(
-    MODEL_ID_BASIC,
-    'Basic Model',
-    fields=[
-        {'name': 'Front'},
-        {'name': 'Back'},
-        {'name': 'UID'},
-    ],
-    templates=[
-        {
-            'name': 'Basic Card',
-            'qfmt': '{{Front}}',
-            'afmt': '{{FrontSide}}<hr id="answer">{{Back}}',
-        },
-    ],
-    css="""
-    .card {
-      font-family: arial;
-      font-size: 20px;
-      text-align: left;
-      color: black;
-      background-color: white;
-    }
-    """
-)
-
-cloze_model = genanki.Model(
-    MODEL_ID_CLOZE,
-    'Cloze Model',
-    fields=[
-        {'name': 'Text'},
-        {'name': 'BackExtra'},
-        {'name': 'UID'},
-    ],
-    templates=[
-        {
-            'name': 'Cloze Card',
-            'qfmt': '{{cloze:Text}}',
-            'afmt': '{{cloze:Text}}<br>{{BackExtra}}',
-        },
-    ],
-    css=basic_model.css,
-    model_type=genanki.Model.CLOZE,
-)
 
 # Create top-level package
 package = genanki.Package([])
